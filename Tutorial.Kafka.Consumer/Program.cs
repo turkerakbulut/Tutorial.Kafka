373f812adca2bc343f9fd3a812a45e260d074d48
turkerakbulut <turkerakbulut@gmail.com>
2020-11-29 02:28:52 +0300

    whoaa

---
+++ Tutorial.Kafka.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial.Kafka.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "192.168.40.133:9092",
+                GroupId = "consumer1", //Consumer ID 
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                ClientId = Dns.GetHostName()
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+               
+            }
+        }
+    }
+}
---
+++ Tutorial.Kafka.Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+
+namespace Tutorial.Kafka.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "192.168.40.133:9092",
+                ClientId = Dns.GetHostName(),
+            };
+
+            for (int i = 0; i < 100; i++)
+            {
+                using (var producer = new ProducerBuilder<string, string>(config).Build())
+                {
+                    producer.Produce("sample", new Message<string, string> {Key=i.ToString() , Value = DateTime.Now.ToString() });
+                    producer.Flush(new TimeSpan(1000));
+                }
+            }
+        }
+    }
+}
